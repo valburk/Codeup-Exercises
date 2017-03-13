2a0860da75b739e7f82b3bd9bf3db891e90a89b7
Valerie Burk <valerieburk@aim.com>
2017-03-13 15:58:47 -0500

    adding new exercises!

---
+++ do-while.php
@@ -0,0 +1,21 @@
+<?php
+
+// $start = 0;
+// do {
+//     echo " {$start}\n";
+//     $start+=2;
+// } while ($start <=  100);
+
+
+// $five = 100;
+// do {
+//     echo " {$five}\n";
+//     $five -=5;
+// } while ($five >=  -10);
+
+$five = 2;
+do {
+    echo " {$five}\n";
+    $five *= $five;
+} while ($five <= 65539);
+
