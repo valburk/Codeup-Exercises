fd7cc2b10db013c108bd1bc3c9cd73d9c6723c9a
Valerie Burk <valerieburk@aim.com>
2017-03-15 11:13:51 -0500

    Adding step 4

--- for.php
+++ for.php
@@ -29,11 +29,22 @@
 
 
 $min = trim(fgets(STDIN));
+if (!is_numeric($min)){
+	 echo "Enter a number";
+	 exit(1);
+}
+
 $max = trim(fgets(STDIN));
+if (!is_numeric($max)){
+	 echo "Enter a number";
+	 exit(1);
+}
 $inc = trim(fgets(STDIN));
 if (!is_numeric($inc)){
 	 $inc = 1;
 }
+
+
 
 for ($i = $min; $i <= $max; $i += $inc) {
     echo "$i\n";
