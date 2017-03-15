aa1d396891f1d603586bd0ab630698d5acc73d71
Valerie Burk <valerieburk@aim.com>
2017-03-15 15:22:07 -0500

    Adding Book exercise step 2

--- for.php
+++ for.php
@@ -1,33 +1,5 @@
 <?php
 
-// if($argc == 3) {
-
-// 	if(is_numeric($argv[1]) && is_numeric($argv[2])) {
-		
-// 		$min = $argv[1];
-// 		$max = $argv[2];
-// 	} else {
-// 		// prompt the user for a numeric minimum until they provide a numeric minimum
-// 		do {
-// 			fwrite(STDOUT, "Minimum");
-// 			$min = trim(fgets(STDIN));
-// 		} while(!is_numeric($min));
-
-// 		// prompt for a maximum that is numeric
-// 		do {
-// 			fwrite(STDOUT, "Maximum");
-// 			$max = trim(fgets(STDIN));
-// 		} while(!is_numeric($max));
-// 	}
-
-// } else {
-// // 	$min = 1;
-// // 	$max = fgets(STDIN);
-// }
-
-
-
-
 $min = trim(fgets(STDIN));
 if (!is_numeric($min)){
 	 echo "Enter a number";
@@ -43,7 +15,6 @@ $inc = trim(fgets(STDIN));
 if (!is_numeric($inc)){
 	 $inc = 1;
 }
-
 
 
 for ($i = $min; $i <= $max; $i += $inc) {
