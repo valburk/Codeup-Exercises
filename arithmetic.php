2241153cb0b929e0e9f1bd865596d4d471726904
Valerie Burk <valerieburk@aim.com>
2017-03-15 16:28:04 -0500

    Adding changes

--- arithmetic.php
+++ arithmetic.php
@@ -7,17 +7,17 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    // Add code here
+   return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a*$b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+return $a/$b; // Add code here
 }
 
 // Add code to test your functions here
