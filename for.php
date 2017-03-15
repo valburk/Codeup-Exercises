cc0a67a64b17429cd9ca3a857761e17429eecaf4
Valerie Burk <valerieburk@aim.com>
2017-03-15 11:03:58 -0500

    Adding new exercise

--- for.php
+++ for.php
@@ -1,7 +1,36 @@
 <?php
 
-$input = fgets(STDIN);
+// if($argc == 3) {
 
-for ($i = $input; $i <= $input; $i += $input) {
-    echo "$i";
+// 	if(is_numeric($argv[1]) && is_numeric($argv[2])) {
+		
+// 		$min = $argv[1];
+// 		$max = $argv[2];
+// 	} else {
+// 		// prompt the user for a numeric minimum until they provide a numeric minimum
+// 		do {
+// 			fwrite(STDOUT, "Minimum");
+// 			$min = trim(fgets(STDIN));
+// 		} while(!is_numeric($min));
+
+// 		// prompt for a maximum that is numeric
+// 		do {
+// 			fwrite(STDOUT, "Maximum");
+// 			$max = trim(fgets(STDIN));
+// 		} while(!is_numeric($max));
+// 	}
+
+// } else {
+// // 	$min = 1;
+// // 	$max = fgets(STDIN);
+// }
+
+
+
+
+$min = trim(fgets(STDIN));
+$max = trim(fgets(STDIN));
+
+for ($i = $min; $i <= $max; $i++) {
+    echo "$i\n";
 }
