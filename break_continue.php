6d66bbb5642fc90cb53e8b11c76f83f35cdfcc04
Valerie Burk <valerieburk@aim.com>
2017-03-15 15:45:36 -0500

    Adding break continue pt 2

--- break_continue.php
+++ break_continue.php
@@ -1,11 +1,9 @@
 <?php
 $i = 0;
-for ($i = 0; $i <= 100; $i++){
-
-	if($i % 2 !== 0) {
-		continue;
-	}else{
-		echo "$i\n";
-	}
+for ($i = 0; $i <= 100; $i+=2){
+	echo "$i\n";
+    if ($i == 10) {
+        break;
+    }
 
 }
