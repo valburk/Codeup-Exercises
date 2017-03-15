535c9ddab9f429b97d2b549c377b5819cbdaec26
Valerie Burk <valerieburk@aim.com>
2017-03-15 16:30:59 -0500

    Adding step 4

--- arithmetic.php
+++ arithmetic.php
@@ -4,20 +4,26 @@ function add($a, $b)
 {
     return $a + $b;
 }
+echo add(6, 3);
+
 
 function subtract($a, $b)
 {
    return $a - $b;
 }
+echo subtract(8, 4);
+
 
 function multiply($a, $b)
 {
     return $a*$b;
 }
+echo multiply(2, 2);
+
 
 function divide($a, $b)
 {
 return $a/$b; // Add code here
 }
-
+echo divide(6, 2);
 // Add code to test your functions here
